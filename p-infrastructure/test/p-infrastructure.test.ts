cd70cb7ede97b7e9918c1bcb6370e2230fc14346
waseem syed <waseem.syed465@gmail.com>
2025-07-08 18:59:55 +0100

    cdk installed

---
+++ p-infrastructure/test/p-infrastructure.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as PInfrastructure from '../lib/p-infrastructure-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/p-infrastructure-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new PInfrastructure.PInfrastructureStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
